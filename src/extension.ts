aa2d5d14ca1a3d3136bb1f1b609cfd8fe787ed2f
Robert Murtagh <robert.murtagh@benevolent.ai>
2020-06-28 22:32:06 +0100

    Turn on prettier autoformatting

--- src/extension.ts
+++ src/extension.ts
@@ -1,48 +1,46 @@
-/* --------------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- * ------------------------------------------------------------------------------------------ */
-
-import { workspace, ExtensionContext } from 'vscode';
-
+import { window, workspace, ExtensionContext } from "vscode";
 import {
-	LanguageClient,
-	LanguageClientOptions,
-	Executable,
-	ServerOptions,
-} from 'vscode-languageclient';
+  Executable,
+  ServerOptions,
+  LanguageClient,
+  LanguageClientOptions,
+} from "vscode-languageclient";
 
 let client: LanguageClient;
+const outputChannel = window.createOutputChannel("Unison");
 
 export function activate(_context: ExtensionContext) {
-	let executable: Executable = { command: 'unison-lsp-server' };
-	let serverOptions: ServerOptions = { run: executable, debug: executable };
+  let executable: Executable = { command: "unison-lsp-server" };
+  let serverOptions: ServerOptions = { run: executable, debug: executable };
 
-	// Options to control the language client
-	let clientOptions: LanguageClientOptions = {
-		// Register the server for plain text documents
-		documentSelector: [{ scheme: 'file', language: 'plaintext' }],
-		synchronize: {
-			// Notify the server about file changes to '.clientrc files contained in the workspace
-			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
-		}
-	};
+  // Options to control the language client
+  let clientOptions: LanguageClientOptions = {
+    // Register the server for plain text documents
+    documentSelector: [{ scheme: "file", language: "Unison" }],
+    synchronize: {
+      configurationSection: "unison",
+      // Notify the server about file changes to '.clientrc files contained in the workspace
+      fileEvents: workspace.createFileSystemWatcher("**/.clientrc"),
+    },
+    outputChannel,
+  };
 
-	// Create the language client and start the client.
-	client = new LanguageClient(
-		'unisonLanguage',
-		'Unison',
-		serverOptions,
-		clientOptions
-	);
+  // Create the language client and start the client.
+  client = new LanguageClient(
+    "unisonLanguage",
+    "Unison",
+    serverOptions,
+    clientOptions
+  );
 
-	// Start the client. This will also launch the server
-	client.start();
+  // Start the client. This will also launch the server
+  client.start();
+  outputChannel.appendLine("🚀 Unison Extension has launched");
 }
 
 export function deactivate(): Thenable<void> | undefined {
-	if (!client) {
-		return undefined;
-	}
-	return client.stop();
+  if (!client) {
+    return undefined;
+  }
+  return client.stop();
 }
